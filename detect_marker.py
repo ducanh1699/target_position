7ef2f458ab2fecb1871774d051b119abe0dead61
ducanh1699 <ducanh1699@gmail.com>
2022-07-21 16:20:19 +0700

    update pose

--- detect_marker.py
+++ detect_marker.py
@@ -1,8 +1,11 @@
 # import the necessary packages
 import argparse
 import imutils
+import numpy as np
 import cv2
 import sys
+
+from torch import double
 #import cv2.aruco as aruco
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -40,7 +43,7 @@ ARUCO_DICT = {
 # load the input image from disk and resize it
 print("[INFO] loading image...")
 image = cv2.imread(args["image"])
-# image = imutils.resize(image, width=600)
+# image = imutils.resize(image, width=6000, height=4000)
 # verify that the supplied ArUCo tag exists and is supported by
 # OpenCV
 if ARUCO_DICT.get(args["type"], None) is None:
@@ -51,16 +54,31 @@ if ARUCO_DICT.get(args["type"], None) is None:
 print("[INFO] detecting '{}' tags...".format(args["type"]))
 arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[args["type"]])
 arucoParams = cv2.aruco.DetectorParameters_create()
+# arucoParams.adaptiveThreshConstant = 7
 corners, ids, rejected = cv2.aruco.detectMarkers(image, arucoDict, parameters=arucoParams)
+camera_matrix = np.array([1080.0, 0.0, 2048.0, 0.0, 1080.0, 1080.0, 0.0, 0.0, 1.0]).reshape(3,3)
+camera_dist = np.array([0.0, 0.0, 0.0, 0.0, 0.0])
+markerLength = 2.0
 print(len(corners))
 # verify *at least* one ArUco marker was detected
 if len(corners) > 0:
 	# flatten the ArUco IDs list
 	ids = ids.flatten()
-	# loop over the detected ArUCo corners
+	# for i in range (0, len(ids)):
+		# loop over the detected ArUCo corners
 	for (markerCorner, markerID) in zip(corners, ids):
 		# extract the marker corners (which are always returned in
 		# top-left, top-right, bottom-right, and bottom-left order)
+		ret1 = cv2.aruco.estimatePoseSingleMarkers(corners=markerCorner, markerLength = markerLength, cameraMatrix=camera_matrix, distCoeffs=camera_dist)
+		# print(ret1)
+		rvec, tvec = ret1[0][0, 0, :], ret1[1][0, 0, :]
+		rmat,_ = cv2.Rodrigues(rvec)
+		rmat = np.array(rmat, dtype=np.float32)
+		tvec_inv = np.matmul(-rmat.T, tvec)
+		# print(tvec_inv)
+		# -- Draw the detected marker and put a reference frame over it
+		# cv2.aruco.drawDetectedMarkers(image, corners, ids)
+		cv2.aruco.drawAxis(image, camera_matrix, camera_dist, rvec, tvec, 0.2)
 		corners = markerCorner.reshape((4, 2))
 		(topLeft, topRight, bottomRight, bottomLeft) = corners
 		# convert each of the (x, y)-coordinate pairs to integers
@@ -68,7 +86,7 @@ if len(corners) > 0:
 		bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
 		bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
 		topLeft = (int(topLeft[0]), int(topLeft[1]))
-        		# draw the bounding box of the ArUCo detection
+				# draw the bounding box of the ArUCo detection
 		cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
 		cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
 		cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
@@ -82,6 +100,7 @@ if len(corners) > 0:
 		cv2.putText(image, str(markerID),
 			(topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,
 			0.5, (0, 255, 0), 2)
+		print("Position camera in marker frame: ", tvec_inv)
 		print("[INFO] ArUco marker ID: {}".format(markerID))
 		print("At the position ", cX, cY)
 		# show the output image
